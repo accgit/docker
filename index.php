c39383173671367322719934ce4c14b85207009c
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-07-01 09:45:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
 
-// database login
-$db = [
-	'server'   => 'db:3306',
-	'username' => 'nette',
-	'password' => 'nette'
-];
-
-// database connection
-$conn = mysqli_connect($db['server'], $db['username'], $db['password']);
-$conn ?: die('Connection failed: ' . mysqli_connect_error());
-
-// print echo when everything is fine
-echo '<div style="text-align: center; color: green">Database: Connected successfully</div>';
-
 // php version info
 phpinfo();
